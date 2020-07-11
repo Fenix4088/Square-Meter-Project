ea48dfe6c1214182634b01752fd3e3c19cb4621b
Fenix <fenix4088@gmail.com>
2020-07-11 20:00:04 +0200

    view for listing created

--- src/js/listing/listingController.js
+++ src/js/listing/listingController.js
@@ -1,6 +1,8 @@
+import * as view from "./listingView.js";
+
 export default function (state) {
     console.log("Listing started");
-
+    view.render();
     // Подписка на пользовательское событие
     state.emitter.subscribe("event:render-listing", () => {
         console.log("FUNCTION STARTEd");
