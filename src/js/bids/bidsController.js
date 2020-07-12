19d9da3c6a93d1e91656d77ae0035312cf77425d
Fenix <fenix4088@gmail.com>
2020-07-12 20:50:06 +0200

    Functions for render bids

--- src/js/bids/bidsController.js
+++ src/js/bids/bidsController.js
@@ -2,11 +2,11 @@ import * as view from "./bidsView.js";
 import Bids from "./bidsModel.js";
 
 export default async function (state) {
-    // Отображение контейнера
-    view.renderContainer();
     // Создаем обьект для bids в state
     if (!state.bids) state.bids = new Bids();
 
     await state.bids.getBids();
+    // Отображение заявок
+    view.renderBids(state.bids.bids)
     
 }
--- src/js/bids/bidsView.js
+++ src/js/bids/bidsView.js
@@ -1,7 +1,7 @@
 // Ф-я рендеринга контейнера
-export function renderContainer () {
+function renderContainer () {
     const markup = `
-            <div class="container p-0 mb-5">
+            <div id="bidsHolder" class="container p-0 mb-5">
                 <div class="heading-1">Заявки</div>
             </div>
 
@@ -14,4 +14,26 @@ export function renderContainer () {
     `;      
 
     document.querySelector('#app').insertAdjacentHTML("afterbegin", markup);
+}
+
+// Ф-я для рендеринга заявок
+function renderBid (bid) {
+    const markup = `
+    <div class="panel panel--no-hover">
+    <div class="bidid">${bid.id}</div>
+        <div class="panel__bidname">${bid.name}</div>
+        <div class="panel__bidphone">${bid.phone}</div>
+    </div>
+    `;
+
+
+    document.querySelector('#bidsHolder').insertAdjacentHTML('beforeend', markup);
+}
+
+// Ф-я которая рендерит сначало контейнер, а потом будет рендерить все заявки
+export function renderBids (bids) {
+    renderContainer();
+    bids.forEach( item => {
+        renderBid(item);
+    });
 }
