64c1f02e651a6a58fc4b3e267ea23fac6d42d13e
Fenix <fenix4088@gmail.com>
2020-07-12 12:53:49 +0200

    Show and hide modal window functions created

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -7,4 +7,25 @@ export default async function (state) {
     await state.singleItem.getItem();
     // Рендерим разметку для отдельного обьекта
     view.render(state.singleItem.result);
+
+    // Прослушка событий
+
+    // Открытие модального окна
+    document.querySelector(".button-order").addEventListener("click", () => {
+        view.showModal();
+    });
+
+    // Скрытие модального окна - клик по кнопке
+    document.querySelector(".modal__close").addEventListener("click", () => {
+        view.hideModal();
+    });
+
+    // Скрытие модального окна - клик оверлею
+    document.querySelector(".modal-wrapper").addEventListener("click", (e) => {
+        if (e.target.closest(".modal")) {
+            return null;
+        } else {
+            view.hideModal();
+        }
+    });
 }
