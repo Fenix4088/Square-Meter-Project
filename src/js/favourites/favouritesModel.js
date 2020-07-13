6f38995fe6538a8a851b0caa8021e0a7d023975c
Fenix <fenix4088@gmail.com>
2020-07-13 10:49:45 +0200

    LS for favourites ready and updating

--- src/js/favourites/favouritesModel.js
+++ src/js/favourites/favouritesModel.js
@@ -1,16 +1,19 @@
 export default class Favourites {
     constructor() {
         this.favs = [];
-        // TODO: работа с LS
+        // работа с LS
+        this.readStorage()
     }
     // Метод для добавления id текущего элемента в массив state
     addFav(id) {
         this.favs.push(id);
+        this.saveData();
     }
     // Метод для удаления из избранного
     removeFav(id) {
         const index = this.favs.indexOf(id);
         this.favs.splice(index, 1);
+        this.saveData();
     }
 
     // Ф-я для проверки наличия элемента в массиве с избранным
@@ -20,5 +23,16 @@ export default class Favourites {
     // Метод который включает либо добавление либо удаление
     toggleFav(id) {
         this.isFav(id) ? this.removeFav(id) : this.addFav(id);
+    }
+
+    // Метод для работы с LS
+    saveData() {
+        localStorage.setItem('favs', JSON.stringify(this.favs));
+    }
+
+    // Метод для инициализации LS при первой загрузке
+    readStorage () {
+        const storage = JSON.parse(localStorage.getItem('favs'));
+        if (storage) this.favs = storage;
     }
 }
